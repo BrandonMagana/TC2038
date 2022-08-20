5bee71002e7baf1d4dc4e89dee3d13d908c2c94a
BrandonMagana <magana.brandon12@outlook.com>
2022-08-20 13:12:52 -0500

    MergeSort in descending order

--- MergeSort/main.cpp
+++ MergeSort/main.cpp
@@ -4,14 +4,14 @@
 
 using namespace std;
 
-vector<int> merge(vector<int> vect1, vector<int> vect2){
-    vector<int> output;
+vector<double> merge(vector<double> vect1, vector<double> vect2){
+    vector<double> output;
     int i = 0, j = 0;
     int vect1_size = vect1.size();
     int vect2_size = vect2.size();
 
     while(i < vect1_size and j < vect2_size){
-        if(vect1[i] < vect2[j]){
+        if(vect1[i] > vect2[j]){
             output.push_back(vect1[i]);
             i++;
         }else{
@@ -33,25 +33,25 @@ vector<int> merge(vector<int> vect1, vector<int> vect2){
     return output;
 }
 
-vector<int> mergeSort(vector<int> vect){
+vector<double> mergeSort(vector<double> vect){
     if(vect.size() == 1)
         return vect;
     
     int middle = vect.size() / 2;
 
-    vector<int> left_side = vector<int>(vect.begin(), vect.begin() + middle);
-    vector<int> right_side = vector<int>(vect.begin()+ middle, vect.end());
+    vector<double> left_side = vector<double>(vect.begin(), vect.begin() + middle);
+    vector<double> right_side = vector<double>(vect.begin()+ middle, vect.end());
 
-    vector<int> vect1 = mergeSort(left_side);
-    vector<int> vect2 = mergeSort(right_side);
+    vector<double> vect1 = mergeSort(left_side);
+    vector<double> vect2 = mergeSort(right_side);
     
     return merge(vect1, vect2);
 }
 
-vector<int> createVector(int n){
-    vector<int> new_vect;
+vector<double> createVector(int n){
+    vector<double> new_vect;
     if(n > 0){
-        int num;
+        double num;
         for(int i=0; i < n; i++){
             cin >> num;
             new_vect.push_back(num);
@@ -60,7 +60,7 @@ vector<int> createVector(int n){
     return new_vect;
 }
 
-void printVector(vector<int> vect){
+void printVector(vector<double> vect){
     int vector_size = vect.size();
     for(int i = 0; i < vector_size; i++){
         if(i != vector_size - 1)
@@ -84,8 +84,8 @@ int main(){
         return 0;
     }
 
-    vector<int> vect = createVector(vector_length); 
-    vector<int> result = mergeSort(vect);
+    vector<double> vect = createVector(vector_length); 
+    vector<double> result = mergeSort(vect);
     
     cout<<"Vector Original: "<<endl;
     printVector(vect);
